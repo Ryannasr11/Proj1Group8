dd8647d6ed830bba7fe1adc7e70b9d5240a2be72
Ryan Nasr <ryannasr@lawn-143-215-56-16.lawn.gatech.edu>
2024-02-05 23:19:26 -0500

    temp changes

--- app/src/main/java/com/example/project1group8/ui/US1/USOneFragment.java
+++ app/src/main/java/com/example/project1group8/ui/US1/USOneFragment.java
@@ -4,34 +4,84 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.project1group8.R;
 import com.example.project1group8.databinding.FragmentSlideshowBinding;
 
 public class USOneFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
+    private EditText courseNumberInput;
+    private EditText timeInput;
+    private EditText courseNameInput;
+    private Button submitButton;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        USOneViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(USOneViewModel.class);
+        // ... (other fields)
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        private Button addButton;
+        private Button editButton;
+        private Button deleteButton;
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        // ... (onCreateView method)
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 ViewGroup container, Bundle savedInstanceState) {
+            // ... (existing code)
+
+            // Initialize buttons
+            addButton = binding.addButton; // Make sure you have this in your layout
+            editButton = binding.editButton; // Make sure you have this in your layout
+            deleteButton = binding.deleteButton; // Make sure you have this in your layout
+
+            // Set onClickListener for Add button
+            addButton.setOnClickListener(v -> {
+                addCourse();
+            });
+
+            // Set onClickListener for Edit button
+            editButton.setOnClickListener(v -> {
+                editCourse();
+            });
+
+            // Set onClickListener for Delete button
+            deleteButton.setOnClickListener(v -> {
+                deleteCourse();
+            });
+
+            return root;
+        }
+
+        private void addCourse() {
+            // Code to add a new course to the database
+            // You might want to collect data from the EditText fields
+        }
+
+        private void editCourse() {
+            // Code to edit an existing course in the database
+            // You will need to have a way to select which course to edit
+        }
+
+        private void deleteCourse() {
+            // Code to delete a course from the database
+            // Similarly, you'll need to know which course to delete
+        }
+
+        // ... (other overridden methods)
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            binding = null;
+        }
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
